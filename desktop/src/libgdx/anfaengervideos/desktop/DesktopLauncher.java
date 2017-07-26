565fca8760bda3626e3fa2f6150cf7587d4dc7b8
Thomas Pronold <mail@tomgrill.de>
2017-07-26 13:09:43 +0200

    initial commit

---
+++ desktop/src/libgdx/anfaengervideos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package libgdx.anfaengervideos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import libgdx.anfaengervideos.LibGDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXGame(), config);
+	}
+}
---
+++ html/src/libgdx/anfaengervideos/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package libgdx.anfaengervideos.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import libgdx.anfaengervideos.LibGDXGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LibGDXGame();
+        }
+}
